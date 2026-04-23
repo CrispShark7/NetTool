1b195201ab4a16fe32cdf6fc2463daaea5ef4ef3
CrispShark7 <144424283+CrispShark7@users.noreply.github.com>
2026-04-23 20:03:13 +0800

    Update Extra.py

--- Extra.py
+++ Extra.py
@@ -6,11 +6,11 @@ def capture_file(file_path, platform):
             sys.exit(f"Singbox only supports JSON File: {file_path.suffix}")
         return [file_path]
     if file_path.is_dir():
-        files = [f for f in file_path.iterdir() if f.is_file()]
+        file = [file for file in file_path.iterdir() if file.is_file()]
         if platform == "Singbox":
-            files = [f for f in files if f.suffix == ".json"]
-        files = sorted(files)
-        if not files:
-            sys.exit(f"No File Found in Directory: {file_path}")
-        return files
+            file = [file for file in file if file.suffix == ".json"]
+        file = sorted(file)
+        if not file:
+            sys.exit(f"No File Found in: {file_path}")
+        return file
     sys.exit(f"{file_path} Unknown Type.")
