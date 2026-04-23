dcbf476954bbb8505329cfdda18ec19ef4d42d83
CrispShark7 <144424283+CrispShark7@users.noreply.github.com>
2026-04-23 19:56:18 +0800

    Update Extra.py

--- Extra.py
+++ Extra.py
@@ -8,9 +8,9 @@ def capture_file(file_path, platform):
     if file_path.is_dir():
         file = [file for file in file_path.iterdir() if file.is_file()]
         if platform == "Singbox":
-            file = [file for file in file if file.suffix == ".json"]
+            files = [f for f in files if f.suffix == ".json"]
         file = sorted(file)
         if not file:
-            print(f"No File Found in: {file_path}")
-        return sorted(file)
+            sys.exit(f"No File Found in Directory: {file_path}")
+        return file
     sys.exit(f"{file_path} Unknown Type.")
